e9d55a41f1e3afc2328fd75ff96449baccaf14da
David Pugh <plastonick@gmail.com>
2025-12-07 15:13:29 +0000

    adding some transpose/rotate methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
+pub fn rotate_90<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    transpose(v)
+        .into_iter()
+        .map(|mut r| {
+            r.reverse();
+            r
+        })
+        .collect()
+}
+
+pub fn rotate_270<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    transpose(
+        v.into_iter()
+            .map(|mut r| {
+                r.reverse();
+                r
+            })
+            .collect(),
+    )
+}
