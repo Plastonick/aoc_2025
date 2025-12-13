f369203df835458e06df37e6ffccf4cec901eea1
David Pugh <plastonick@gmail.com>
2025-12-13 20:53:24 +0000

    oops, that should be 33

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -160,6 +160,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(333));
+        assert_eq!(result, Some(33));
     }
 }
